decab777b0e06919a8ec42e62602ef9aa8897b9c
brent sieberg <siebergb@gmail.com>
2025-03-23 13:02:33 -0500

    Add project files.

---
+++ IntegrationTests/Factories/ExampleAppFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace IntegrationTests.Factories
+{
+    [CollectionDefinition(nameof(TestCollection))]
+    public class TestCollection : ICollectionFixture<ExampleAppFactory>;
+
+    public class ExampleAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+    {
+        public HttpClient Client { get; private set; } = default!;
+        public ExampleAppFactory()
+        {
+            Client = Server.CreateClient();
+        }
+
+        public Task InitializeAsync()
+        {
+            Console.WriteLine("InitializeAsync hit.");
+            return Task.CompletedTask;
+        }
+
+        Task IAsyncLifetime.DisposeAsync()
+        {
+            Console.WriteLine("DisposeAsync hit.");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ IntegrationTests/Tests2.cs
@@ -0,0 +1,30 @@
+﻿using ExampleApp;
+using IntegrationTests.Factories;
+using System.Text.Json;
+
+namespace IntegrationTests
+{
+    [Collection(nameof(TestCollection))]
+    public class Tests2 : IClassFixture<ExampleAppFactory>
+    {
+        private readonly ExampleAppFactory _factory;
+        
+        public Tests2(ExampleAppFactory factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Test()
+        {
+            var response = await _factory.Client.GetAsync("/WeatherForecast");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var weatherForecasts = JsonSerializer.Deserialize<IEnumerable<WeatherForecast>>(content);
+
+            Assert.NotNull(weatherForecasts);
+            Assert.NotEmpty(weatherForecasts);
+        }
+    }
+}
